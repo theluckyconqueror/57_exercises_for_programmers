01bc4766ad9ac270a823e8e774ea17adaf8b92ee
Lucky Conqueror <theluckyconqueror@gmail.com>
2020-10-12 21:53:36 -0700

    Meet exercise constraints
    This separates out the input, string output, and string concatenation. The input is handled inside an if-block that only runs when run from the command-line.
    The string concatenation is happens inside its own private method. The output happens in the public method, run.
    
    This is causing the test to fail because the last thing returned from `puts` is nil so I need to fix the tests now.

--- exercise_1/greetings.rb
+++ exercise_1/greetings.rb
@@ -4,6 +4,20 @@ class Greet
   end
 
   def run
-    "Hello, #{@name}, nice to meet you!"
+    greeting = generate_string
+    puts greeting
   end
+
+  private
+
+  def generate_string
+    "Hello, " + "#{@name}" + ", nice to meet you!"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  print "What's your name? "
+  name = gets.chomp
+  greeter = Greet.new name
+  greeter.run
 end
