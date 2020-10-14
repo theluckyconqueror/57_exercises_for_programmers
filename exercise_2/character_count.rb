bf8ac67c909efe33d7868684e164bd36781b3570
Lucky Conqueror <theluckyconqueror@gmail.com>
2020-10-13 18:30:26 -0700

    Fix test_letters_and_spaces
    We do not want to count white space. This uses a regex to remove the spaces from a string.

--- exercise_2/character_count.rb
+++ exercise_2/character_count.rb
@@ -1,5 +1,12 @@
 class CharacterCounter
   def run(characters)
-    characters.length
+    cleaned_characters = remove_spaces(characters)
+    cleaned_characters.length
+  end
+
+  private
+
+  def remove_spaces(string)
+    string.gsub(/ /, "")
   end
 end
