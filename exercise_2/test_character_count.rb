15d2a81eb6bacdb1324383ddb1cc5e2b5e23c457
Lucky Conqueror <theluckyconqueror@gmail.com>
2020-10-13 19:07:14 -0700

    Add test for the output and refactor old tests
    Added tests for output since I was only testing for the correct character count before.

--- exercise_2/test_character_count.rb
+++ exercise_2/test_character_count.rb
@@ -12,36 +12,67 @@ class CharacterCounterTests < Minitest::Test
 
   def test_only_letters
     characters = "LC"
-    assert_equal 2, @counter.run(characters)
+    assert_equal 2, @counter.count(characters)
+  end
+
+  def test_only_letters_output
+    characters = "LC"
+    assert_output(/LC has 2 characters./) { @counter.run(characters) }
   end
 
   def test_letters_and_spaces
     characters = "LC Was Here"
-    assert_equal 9, @counter.run(characters)
+    assert_equal 9, @counter.count(characters)
+  end
+
+  def test_letters_and_spaces_output
+    characters = "LC Was Here"
+    assert_output(/LC Was Here has 9 characters./) { @counter.run(characters) }
   end
 
   def test_empty_string
     characters = ""
-    assert_output(/Please enter some data/) { @counter.run(characters) }
+    assert_output(/Please enter some data./) { @counter.run(characters) }
   end
 
   def test_letters_and_numbers
     characters = "LC 2020"
-    assert_equal 6, @counter.run(characters)
+    assert_equal 6, @counter.count(characters)
+  end
+
+  def test_letters_and_numbers_output
+    characters = "LC 2020"
+    assert_output(/LC 2020 has 6 characters./) { @counter.run(characters) }
   end
 
   def test_letters_symbols
     characters = "LC!"
-    assert_equal 3, @counter.run(characters)
+    assert_equal 3, @counter.count(characters)
   end
 
-  def test_leters_numbers_and_symbols
+  def test_letters_symbols_output
+    characters = "LC!"
+    assert_output(/LC! has 3 characters./) { @counter.run(characters) }
+  end
+
+  def test_letters_numbers_and_symbols
     characters = "LC 2020!"
-    assert_equal 7, @counter.run(characters)
+    assert_equal 7, @counter.count(characters)
+  end
+
+  def test_letters_numbers_and_symbols_output
+    characters = "LC 2020!"
+    assert_output(/LC 2020! has 7 characters./) { @counter.run(characters) }
   end
 
   def test_letters_and_emoji
     characters = "LC ✅"
-    assert_equal 3, @counter.run(characters)
+    assert_equal 3, @counter.count(characters)
   end
+
+  def test_letters_and_emoji_output
+    characters = "LC ✅"
+    assert_output(/LC ✅ has 3 characters/) { @counter.run(characters) }
+  end
+
 end
