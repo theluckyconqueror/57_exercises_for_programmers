5abb9ea25f490a7d21a3f8067a4a3af2ef4d7c3e
Lucky Conqueror <theluckyconqueror@gmail.com>
2020-10-13 18:24:34 -0700

    Flesh out the test
    Added all of the assertions for the tests.

--- exercise_2/test_character_count.rb
+++ exercise_2/test_character_count.rb
@@ -16,37 +16,43 @@ class CharacterCounterTests < Minitest::Test
   end
 
   def test_letters_and_spaces
-    skip
     characters = "LC Was Here"
+    assert_equal 9, @counter.run(characters)
   end
 
   def test_no_input
     skip
     characters = nil
+    assert_output(/Please enter some data/) { @counter.run(characters) }
   end
 
   def test_empty_string
     skip
     characters = ""
+    assert_output(/Please enter some data/) { @counter.run(characters) }
   end
 
   def test_letters_and_numbers
     skip
     characters = "LC 2020"
+    assert_equal 6, @counter.run(characters)
   end
 
   def test_letters_symbols
     skip
     characters = "LC!"
+    assert_equal 3, @counter.run(characters)
   end
 
   def test_leters_numbers_and_symbols
     skip
     characters = "LC 2020!"
+    assert_equal 7, @counter.run(characters)
   end
 
   def test_letters_and_emoji
     skip
     characters = "LC ✅"
+    assert_equal 3, @counter.run(characters)
   end
 end
